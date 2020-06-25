0d0985da4ee7355632b3a1d528cccd274304b57c
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 18:26:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
 
-  def initialize(name)
-    @name = name
-  end
+def name=(dogs_name)
+  @name= dogs_name
+end
 
-  def name
-    @name
-  end
+def name
+  @name
+end
+def breed=(breed)
+  @breed = breed
+end
+
 end
